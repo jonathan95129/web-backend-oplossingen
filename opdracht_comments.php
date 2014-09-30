4cd990b9c9995b294e78a9bf1464020e0c5c21eb
Jonathan Put <jonathan95129@hotmail.com>
2014-09-30 10:30:54 +0200

    opdracht 1 toegevoegd

---
+++ opdracht_comments.php
@@ -0,0 +1,27 @@
+<?php
+	
+	$naam		=	"jonathan";
+	# jonathan.put@student.kdg.be
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>comments</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="author" href="humans.txt">
+    </head>
+    <body>
+        
+        <h1>Opdracht comments: deel 1</h1>
+
+
+        <?= $naam ?>
+
+        
+        
+    </body>
+</html>
