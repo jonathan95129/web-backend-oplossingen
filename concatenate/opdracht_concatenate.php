e554a6d9831cb5913581a4e41200a7394a122e56
Jonathan Put <jonathan95129@hotmail.com>
2014-09-30 17:18:02 +0200

    oefeningen toegevoegd

---
+++ concatenate/opdracht_concatenate.php
@@ -0,0 +1,26 @@
+<?php
+	
+	$Voornaam		=	"jonathan";
+	$Achternaam		=	"put";
+	$volledigeNaam = $Voornaam.$Achternaam;
+	$volledigeNaamLengte = strlen($volledigeNaam);
+
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>oplossing</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="author" href="humans.txt">
+    </head>
+    <body>
+        
+        <?= $volledigeNaam ?> <br>
+        <?= $volledigeNaamLengte ?>
+    </body>
+</html>
